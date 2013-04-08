bf53d72a4a91830aa8259720c9931189b864ff0e
Jason <jason@addepar.com>
2013-04-07 17:34:07 -0700

    Initial Commit

---
+++ src/sprites/SpriteAnimator.java
@@ -0,0 +1,15 @@
+package sprites;
+
+import javax.swing.JFrame;
+
+public class SpriteAnimator extends JFrame {
+
+  public SpriteAnimator() {
+
+  }
+
+  public static void main(String[] args) {
+    new SpriteAnimator();
+  }
+
+}
